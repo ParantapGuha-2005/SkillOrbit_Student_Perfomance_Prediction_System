ee0d02e6fa7535903e1792c53b562bb0b287213e
Parantap Guha <parantapguha@gmail.com>
2026-08-06 04:18:42 +0530

    Update markdown styles in app.py
    Adjust margin and line-height styles in markdown.

--- app.py
+++ app.py
@@ -541,8 +541,8 @@ def page_factor_analysis(df):
             f"""
             <h3 style="
                 margin-top: 0rem;
-                margin-bottom: -0.1rem;
-                line-height: 1.2;
+                margin-bottom: 0rem;
+                line-height: 1;
                 font-size: calc(1.3rem + 0.6vw);
                 font-weight: 600;
                 font-family: inherit;
