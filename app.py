c1eb31a833f7d28a68cdacbe50e8bb4c8cd91b26
Parantap Guha <parantapguha@gmail.com>
2026-08-06 06:29:49 +0530

    Increase max-height of images to 45vh

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ def page_overview(df, comparison_df):
                 margin: 0.2rem 0 0.6rem 0 !important;
             }
             div[data-testid="stImage"] img {
-                max-height: 38vh;
+                max-height: 45vh;
                 width: auto;
                 margin: 0 auto;
                 display: block;
